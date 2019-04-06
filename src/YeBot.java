4ae38604a93a8d8b1ea53d35072f1eb54ad31358
Courtney Gosselin <courtneygosselin1996@gmail.com>
2019-04-05 19:58:13 -0700

    YeBot now has synonym recognition
    Signed-off-by: Courtney Gosselin <courtneygosselin1996@gmail.com>

--- src/YeBot.java
+++ src/YeBot.java
@@ -1,13 +1,21 @@
-import java.util.*;
-
-import opennlp.tools.postag.*;
-import opennlp.tools.stemmer.PorterStemmer;
-import org.alicebot.ab.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+
+import org.alicebot.ab.Bot;
+import org.alicebot.ab.Chat;
+import org.alicebot.ab.MagicBooleans;
+
+import edu.smu.tspell.wordnet.*;
+//import net.didion.jwnl.JWNLException;
+//import net.didion.jwnl.data.Synset;
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSTaggerME;
+import opennlp.tools.stemmer.PorterStemmer;
+//import edu.mit.jwi.Dictionary;
 /**
  * YeBot is the main class where the conversation is held.
  */
@@ -15,29 +23,51 @@ public class YeBot {
 
 	static Conversation conversation;			
 
-	public static void main(String[] args) throws FileNotFoundException, IOException {
+	public static void main(String[] args) throws FileNotFoundException, IOException{
+	
+	
+		Bot yebot = null;
+		ArrayList<String> unknowns;
+		Chat session;
+		
+		WordNetDatabase database = null;
+		
+		//Note you need to have wordnet installed for this program to work with it
+		try{
+			System.setProperty("wordnet.database.dir", "C:\\Program Files (x86)\\WordNet\\2.1\\dict\\");
+			database = WordNetDatabase.getFileInstance();
+		}catch(Exception e){
+			System.out.println("Download princetons word net to run the synonns properly. See Yebot.java ");
+		}
+		
 		
 		PorterStemmer stemmer = new PorterStemmer();
-		
+	
 		//initialize
 		String dir = new File(".").getAbsolutePath();
-		System.out.println(dir.substring(0,dir.length()-2));
+		System.out.println(dir);
+		System.out.println(dir.substring(0,dir.length()-1));
+		//dir = dir.substring(0,dir.length()-1)+"bots\\YeBot\\aiml";
 		MagicBooleans.trace_mode = false;
-		Bot yebot = new Bot("YeBot",dir.substring(0,dir.length()-2));
+		//yebot = new Bot("YeBot", dir);
+		yebot = new Bot("YeBot", dir.substring(0,dir.length()-1));
 		yebot.writeAIMLFiles();
 		String ans;
-		
-		Chat session = new Chat(yebot);
-		conversation = new Conversation();
-
-		String input = "test";
-		String output= null;
-		ArrayList<String> unknowns = new ArrayList<String>();
+				
+		//Arraylist of default answers 
+		unknowns = new ArrayList<String>();
 		unknowns.add("Wish I could help I dont know what that means");
 		unknowns.add("You got good vibes but I dont know what to say to that");
 		unknowns.add("Yo man you gotta slow down maybe try saything that a different way");
 		unknowns.add("That aint something im knowledgable on maybe say it ina different way");
 		unknowns.add("Could you say it in a different way I may just not know what youre on about");
+				
+	
+		session = new Chat(yebot);
+		conversation = new Conversation();
+
+		String input = "test";
+		String output= null;
 		//String response;
 		int i = 1;
 			
@@ -63,18 +93,44 @@ public class YeBot {
 					break;
 				}else{
 					//regular response
+					branch:
 					if(unknowns.contains(session.multisentenceRespond(input))){
+						String sent[] =  input.split(" ");
+						
+						try{
+							for(int k= 0; k <sent.length; k++){
+								Synset[] syns = database.getSynsets(sent[k]);
+								if(syns.length!=0){
+									for(Synset syn : syns){
+										String[] synonym = syn.getWordForms(); //cursed
+										for(String s : synonym){
+											String newInput = input.replaceAll(sent[k]+" ", s+" ");
+											output = conversation.response(session.multisentenceRespond(newInput));
+											if(!unknowns.contains(output)){
+												break branch;
+											}
+											
+										}
+										
+									}
+								}
+							}
+						}catch(Exception e){
+							System.out.println("Wordnet issue "+ e);
+						}
 						//POS use to respond if we are given a default response and the input contains a noun
 						try(InputStream modelIn = new FileInputStream("res/en-pos-maxent.bin")){
 							POSModel model = new POSModel(modelIn);
 							POSTaggerME tagger = new POSTaggerME(model);
-							String sent[] =  input.split(" ");
+						
 							String tags[] = tagger.tag(sent);
 							//System.out.println(tags[0]);
-							for(int j = 0; j <= tags.length; j++){
+							for(int j = 0; j < tags.length; j++){
 								if(tags[j].equals("NN") || tags[j].equals("NNP")){
 									output = conversation.response("I ain't know nothing bout " + sent[j]);
 									break;
+								}else{
+									output = conversation.response(session.multisentenceRespond(input));
 								}
 							}
 							
